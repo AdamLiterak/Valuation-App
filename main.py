4d8c99b05e5fcd595324c9a44bb2216ee20107c1
Adam Literak <37661880+AdamLiterak@users.noreply.github.com>
2021-12-07 00:21:34 +0000

    adding layouts

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import yfinance as yf
 
 #export FLASK_APP=main.py
 #q for exiting git commands in terminal
+#token: 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
 
 app = Flask(__name__)
 
@@ -9,9 +11,15 @@ app = Flask(__name__)
 def hello_world():
   return render_template("index.html")
 
-@app.route('/test')
-def hi():
-  return render_template("test.html")
+@app.route('/valuation', methods=["GET", "POST"])
+def valuation():
+  ticker = request.form.get("ticker")
+  if request.method == "POST":
+    
+    return render_template("valuation.html", ticker=ticker)
+
+  else:
+    return render_template("valuation.html", ticker=ticker)
 
 app.run(host='0.0.0.0', port=8080)
 
